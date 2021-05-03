f0ca4bb5cc282cc287bf335f2f92f802c647733b
dadchyyy <dadchyyy@gmail.com>
2021-05-03 20:07:20 +0900

    Automatically updated on 2021-05-03 20;07:20

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-git add $(git ls-files)
-local timestamp=$(date '+%Y-%m-%d %H;%M:%S')
-git commit -m "Automatically updated on ${timestamp}"
+FILES=`git ls-files`
+TIMESTAMP=`date '+%Y-%m-%d %H;%M:%S'`
+
+git add $FILES
+git commit -m "Automatically updated on ${TIMESTAMP}"
 git push
